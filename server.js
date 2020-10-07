ed23dd31decdc09995ef204040dfca3eeac110b8
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-10-06 19:37:19 -0500

    direct to client index.html

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === "production") {
 // });
 
 app.get("*", (req, res) => {
-    res.sendFile('./client/build/client/index.html');
+    res.sendFile('./client/dist/client/index.html');
 });
 
 app.listen(PORT, () => {
