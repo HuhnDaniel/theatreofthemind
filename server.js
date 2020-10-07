0b7d155ac7c6ddd0e2278d11e47a585b4d85396d
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-10-06 19:32:22 -0500

    direct to client index.html

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === "production") {
 // });
 
 app.get("*", (req, res) => {
-    res.sendFile('/index.html');
+    res.sendFile('./client/build/client/index.html');
 });
 
 app.listen(PORT, () => {
