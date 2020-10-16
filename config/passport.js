195e278b69dd61d20afbbc66a154a55a687b63e8
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-10-16 14:12:51 -0500

    rework passport/login

--- config/passport.js
+++ config/passport.js
@@ -1,29 +1,52 @@
-const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
-const User = mongoose.model('User');
+const User = require('../models/user');
 
 passport.use(new LocalStrategy({
-    usernameField: 'user[email]',
-    passwordField: 'user[password]'
+    usernameField: 'email',
 }, (email, password, done) => {
-    User.findOne({ email })
+    User.findOne({ email: email })
         .then((user) => {
-            if(!user || !user.validPW(password)) {
-                return done(null, false, { errors: { 'email or password': 'is invalid' } });
+            if(!user) {
+                const newUser = new User({ email, password });
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                               .then(user => {
+                                   return done(null, user);
+                               })
+                               .catch(err => {
+                                   return done(null, false, { message: err });
+                               });
+                    });
+                });
+            } else {
+                bcrypt.compare(password, user.password, (err, isMatch) => {
+                    if (err) throw err;
+                    if (isMatch) {
+                        return done(null, user);
+                    } else {
+                        return done(null, false, { message: "Wrong Username or Password" });
+                    }
+                });
             }
-
-            return done(null, user);
-        }).catch(done);
+        }).catch(err => {
+            return done(null, false, { message: err });
+        });
 }));
 
-passport.serializeUser((user, cb) => {
-    cb(null, user);
+passport.serializeUser((user, done) => {
+    done(null, user.id);
 });
 
-passport.deserializeUser((obj, cb) => {
-    cb(null, obj);
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
 });
 
 module.exports = passport;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const auth = require('./auth');
 const entityRoutes = require('./encounters');
 const userRoutes = require('./users');
 
+router.use('/auth', auth);
 router.use('/encounters', entityRoutes);
 router.use('/users', userRoutes);
 
